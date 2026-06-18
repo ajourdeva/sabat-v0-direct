d9fa44e11c952df2780a1f9c938f35492cbc9653
v0agent <it+v0agent@vercel.com>
2026-06-18 15:30:45 +0000

    feat: remove custom font imports and styles from layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,25 +1,8 @@
 import React from "react"
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
-import { Instrument_Sans, Instrument_Serif, JetBrains_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import '../globals.css'
-
-const instrumentSans = Instrument_Sans({ 
-  subsets: ["latin"],
-  variable: '--font-instrument'
-});
-
-const instrumentSerif = Instrument_Serif({ 
-  subsets: ["latin"],
-  weight: "400",
-  variable: '--font-instrument-serif'
-});
-
-const jetbrainsMono = JetBrains_Mono({ 
-  subsets: ["latin"],
-  variable: '--font-jetbrains'
-});
 
 export default async function LocaleLayout({
   children,
@@ -38,7 +21,7 @@ export default async function LocaleLayout({
       dir={isRTL ? "rtl" : "ltr"}
       className="bg-background"
     >
-      <body className={`${instrumentSans.variable} ${instrumentSerif.variable} ${jetbrainsMono.variable} font-sans antialiased`}>
+      <body className="font-sans antialiased">
         <NextIntlClientProvider messages={messages} locale={locale}>
           {children}
         </NextIntlClientProvider>
